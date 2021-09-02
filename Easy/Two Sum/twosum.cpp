faed59296f0c19eb1285499ba5ac4ae80dd19142
Amit Dwivedi <amit.dwivedi.nit@gmail.com>
2021-09-02 19:42:12 +0530

    Create twosum.cpp

---
+++ Easy/Two Sum/twosum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i++){
+            if(mp.find(target-nums[i])!=mp.end()){
+                ans = {mp[target-nums[i]],i};
+                break;
+            }
+            mp[nums[i]] = i;
+        }
+        return ans;
+    }
+};
